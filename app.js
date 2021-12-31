9e72c3c1e5e676a1215ae432d22103a41fb4c9a1
Paul Kang <minseok_kang09@naver.com>
2021-12-31 19:26:43 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const days = document.querySelectorAll('days');
+const hours = document.querySelectorAll('hours');
+const minutes = document.querySelectorAll('minutes');
+const seconds = document.querySelectorAll('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date('January 1 $ {currentYear + 1} 00:00:00');
+
+//Update Contdown time
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+
+}
+
+setInterval(updateCountdown, 1000);
